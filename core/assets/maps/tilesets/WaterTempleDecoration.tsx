89c9b87f3d1e029b5db922353aabc5c40262dd54
Galen <galen.scovell@gmail.com>
2017-11-06 20:26:48 -0800

    Add sandbox tiles/tilesets/tilemap

---
+++ core/assets/maps/tilesets/WaterTempleDecoration.tsx
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="WaterTempleDecoration" tilewidth="32" tileheight="32" tilecount="18" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="../tiles/waterTemple/decoration/00 floor1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="../tiles/waterTemple/decoration/01 floor2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="../tiles/waterTemple/decoration/02 floor3_1.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="../tiles/waterTemple/decoration/03 floor3_2.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="../tiles/waterTemple/decoration/04 floor3_3.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="../tiles/waterTemple/decoration/05 floor3_4.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="../tiles/waterTemple/decoration/06 floor4.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="../tiles/waterTemple/decoration/07 floor5.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="../tiles/waterTemple/decoration/08 floor6.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="../tiles/waterTemple/decoration/09 lilypad-0.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="../tiles/waterTemple/decoration/10 lilypad-1.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="../tiles/waterTemple/decoration/11 panel0_0.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="../tiles/waterTemple/decoration/12 panel0_1.png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="../tiles/waterTemple/decoration/13 panel0_2.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="../tiles/waterTemple/decoration/14 panel0_3.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="../tiles/waterTemple/decoration/15 water-grass-0.png"/>
+ </tile>
+ <tile id="16">
+  <image width="32" height="32" source="../tiles/waterTemple/decoration/16 water-grass-1.png"/>
+ </tile>
+ <tile id="17">
+  <image width="32" height="32" source="../tiles/waterTemple/decoration/17 water-grass-2.png"/>
+ </tile>
+</tileset>
---
+++ core/assets/maps/tilesets/WaterTempleGround.tsx
@@ -0,0 +1,91 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="WaterTempleGround" tilewidth="32" tileheight="32" tilecount="29" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="29">
+  <image width="32" height="32" source="../tiles/waterTemple/ground/00 down-1-1.png"/>
+ </tile>
+ <tile id="30">
+  <image width="32" height="32" source="../tiles/waterTemple/ground/01 down-1-2.png"/>
+ </tile>
+ <tile id="31">
+  <image width="32" height="32" source="../tiles/waterTemple/ground/02 down-2-1.png"/>
+ </tile>
+ <tile id="32">
+  <image width="32" height="32" source="../tiles/waterTemple/ground/03 down-2-2.png"/>
+ </tile>
+ <tile id="33">
+  <image width="32" height="32" source="../tiles/waterTemple/ground/04 down-left-diag-1-1.png"/>
+ </tile>
+ <tile id="34">
+  <image width="32" height="32" source="../tiles/waterTemple/ground/05 down-left-diag-1-2.png"/>
+ </tile>
+ <tile id="35">
+  <image width="32" height="32" source="../tiles/waterTemple/ground/06 down-left-diag-2-1.png"/>
+ </tile>
+ <tile id="36">
+  <image width="32" height="32" source="../tiles/waterTemple/ground/07 down-left-diag-2-2.png"/>
+ </tile>
+ <tile id="37">
+  <image width="32" height="32" source="../tiles/waterTemple/ground/08 down-right-diag-1-1.png"/>
+ </tile>
+ <tile id="38">
+  <image width="32" height="32" source="../tiles/waterTemple/ground/09 down-right-diag-1-2.png"/>
+ </tile>
+ <tile id="39">
+  <image width="32" height="32" source="../tiles/waterTemple/ground/10 down-right-diag-2-1.png"/>
+ </tile>
+ <tile id="40">
+  <image width="32" height="32" source="../tiles/waterTemple/ground/11 down-right-diag-2-2.png"/>
+ </tile>
+ <tile id="41">
+  <image width="32" height="32" source="../tiles/waterTemple/ground/12 down-right-dot.png"/>
+ </tile>
+ <tile id="42">
+  <image width="32" height="32" source="../tiles/waterTemple/ground/13 down-left-dot.png"/>
+ </tile>
+ <tile id="43">
+  <image width="32" height="32" source="../tiles/waterTemple/ground/14 down-right-corner.png"/>
+ </tile>
+ <tile id="44">
+  <image width="32" height="32" source="../tiles/waterTemple/ground/15 down-left-corner.png"/>
+ </tile>
+ <tile id="45">
+  <image width="32" height="32" source="../tiles/waterTemple/ground/16 right-1.png"/>
+ </tile>
+ <tile id="46">
+  <image width="32" height="32" source="../tiles/waterTemple/ground/17 right-2.png"/>
+ </tile>
+ <tile id="47">
+  <image width="32" height="32" source="../tiles/waterTemple/ground/18 up-1.png"/>
+ </tile>
+ <tile id="48">
+  <image width="32" height="32" source="../tiles/waterTemple/ground/19 up-2.png"/>
+ </tile>
+ <tile id="49">
+  <image width="32" height="32" source="../tiles/waterTemple/ground/20 up-right-diag.png"/>
+ </tile>
+ <tile id="50">
+  <image width="32" height="32" source="../tiles/waterTemple/ground/21 up-left-diag.png"/>
+ </tile>
+ <tile id="51">
+  <image width="32" height="32" source="../tiles/waterTemple/ground/22 up-right-corner.png"/>
+ </tile>
+ <tile id="52">
+  <image width="32" height="32" source="../tiles/waterTemple/ground/23 up-left-corner.png"/>
+ </tile>
+ <tile id="53">
+  <image width="32" height="32" source="../tiles/waterTemple/ground/24 up-right-dot.png"/>
+ </tile>
+ <tile id="54">
+  <image width="32" height="32" source="../tiles/waterTemple/ground/25 up-left-dot.png"/>
+ </tile>
+ <tile id="55">
+  <image width="32" height="32" source="../tiles/waterTemple/ground/26 left-1.png"/>
+ </tile>
+ <tile id="56">
+  <image width="32" height="32" source="../tiles/waterTemple/ground/27 left-2.png"/>
+ </tile>
+ <tile id="57">
+  <image width="32" height="32" source="../tiles/waterTemple/ground/28 middle.png"/>
+ </tile>
+</tileset>
